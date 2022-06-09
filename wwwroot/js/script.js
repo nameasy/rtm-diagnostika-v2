33df5606c57490503b6d5029089e04559afb9abe
Stepan Lyubavtsev <nameasy@mail.ru>
2022-06-10 02:47:28 +0300

    Add files via upload

---
+++ wwwroot/js/script.js
@@ -0,0 +1,23 @@
+﻿jQuery(function ($) {
+    $(".header").headroom();
+    /* $("#openProfile").on("click", () => {
+        $(".profile").css({
+            "display": "block"
+        });
+        $(".cabinet-body").css({
+            "display": "none"
+        });
+    }); */
+    $("#openProfile").on("click", () => {
+        $(".cabinet").addClass("is-collapsed")
+    });
+    $("#closeProfile").on("click", () => {
+        $(".cabinet").removeClass("is-collapsed")
+    });
+    $("#regNumber, #password").on("input", () => {
+        $("#regNumber").val() && $("#password").val() ? $("#login").addClass("button-primary") : $("#login").removeClass("button-primary")
+    });
+    $("#regNumber, #password1, #password2").on("input", () => {
+        $("#regNumber").val() && $("#password1").val() && $("#password2").val() && ($("#password1").val() === $("#password2").val()) ? $("#registration").addClass("button-primary") : $("#registration").removeClass("button-primary")
+    });
+});
